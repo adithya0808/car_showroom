a2f62629afcd2978beb1c6aa42b3c7072c8ba4aa
My Name <myemail@example.com>
2024-06-07 15:43:53 +0900

    status-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Maintenance from "./Components/Maintenance"
+import "./assets/css/App.css"
+import "./assets/css/Nav.css"
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Maintenance/>
-    </>
-  )
-}
+    <div className="app">
+      <BrowserRouter>
+        <nav className="navbar">
+          <h1 className="nav-title">Car Zone</h1>
+          <ul>
+            <li><Link to="/" className="nav-menu">Home</Link></li>
+            <li><Link to="/" className="nav-menu">About</Link></li>
+            <li><Link to="/" className="nav-menu">Product</Link></li>
+            <li><Link to="/" className="nav-menu">Services</Link></li>
+            <li><Link to="/" className="nav-menu">Contact us</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
